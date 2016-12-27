b31534ff0e00959b26238985822f4ff1245ed22b
Elizabeth Cheng <echeng924@gmail.com>
2016-12-27 18:44:29 -0500

    test fetch function for json pull

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 console.log('connected');
+
+function fetchData() {
+  //fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson')
+  fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson')
+      .then(
+        function(response) {
+          if(response.status !== 200) {
+            console.log('Problem with request ' + response.status);
+            return;
+          }
+        response.json().then(function(data) {
+          console.log(data);
+
+        });
+        }
+      )
+      .catch(function(err) {
+        console.log('Fetch error', err);
+      })
+}
