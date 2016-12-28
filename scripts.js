a5aa35111f4f3884f0fded4ad7c26e37af8dc103
Elizabeth Cheng <echeng924@gmail.com>
2016-12-27 19:06:17 -0500

    add google maps function

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ function fetchData() {
           }
         response.json().then(function(data) {
           console.log(data);
-
+          handleData(data);
         });
         }
       )
@@ -19,3 +19,51 @@ function fetchData() {
         console.log('Fetch error', err);
       })
 }
+
+function handleData(data) {
+  data.features.forEach((earthquake) => {
+    let coordinates = earthquake.geometry.coordinates;
+    let properties = earthquake.properties;
+    let earthquakeCircle = new google.maps.Circle({
+      strokeColor: properties.alert,
+      strokeOpacity: 0.8,
+      strokeWeight: 1,
+      fillColor: properties.alert,
+      fillOpacity: 0.35,
+      map: map,
+      center: {lat: coordinates[1], lng: coordinates[0]},
+      radius: Math.pow(2, properties.mag) * 2000
+    });
+
+    let convertedDate = new Date( properties.time);
+    let infoWindow = new google.maps.InfoWindow({
+      content: `<div><b>Title:</b> ${properties.title}</div>`+
+                `<div><b>Time:</b> ${convertedDate}</div>`+
+                `<div><b>Magnitude:</b> ${properties.mag}</div>`+
+                `<div><b>Tsunami:</b> ${properties.tsunami}</div>`,
+      position: {lat: coordinates[1], lng: coordinates[0]},
+    });
+
+    earthquakeCircle.addListener('click', function() {
+      infoWindow.open(map);
+    })
+
+  });
+  console.log(map)
+}
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.querySelector('#map'), {
+    zoom: 3,
+    center: {lat: 10, lng: 230},
+    mapTypeId: 'terrain'
+  });
+
+  fetchData();
+  let legend = document.getElementById('legend');
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
+}
+
+
